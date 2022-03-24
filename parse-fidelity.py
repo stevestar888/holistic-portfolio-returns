0dd5abde446e6f36cd7dc3be1dcc3e2fea863a9c
Steven Xu <weareallhalfminotaur@gmail.com>
2022-03-23 22:38:08 -0500

    Update parse-fidelity.py

--- parse-fidelity.py
+++ parse-fidelity.py
@@ -3,8 +3,9 @@ import os
 import re
 import regex_store
 import csv
+import datetime
 
-PATH_TO_BROKERAGE_STATEMENTS = "../Brokerage Statements/Fidelity Statements/2021 Monthly"
+PATH_TO_BROKERAGE_STATEMENTS = "../Brokerage Statements/Fidelity Statements/2021 Monthly/"
 
 """
 
@@ -40,7 +41,7 @@ The account value is from the entry for “Ending Account Value”
 """
 
 #subtract 1 b/c PDF pages are 1-indexed
-SCHWAB_ACCOUNT_VALUE_PAGE = 1 - 1
+FIDELITY_ACCOUNT_VALUE_PAGE = 1 - 1
 
 entries = {}
 
@@ -48,62 +49,144 @@ def bookkeep_month_entry(broker, date, account_value, deposits, withdraws):
     entries[date] = [deposits, withdraws, account_value]
 
 
-def parse_date(date):
-    try:
-        parts = date.split("/")
-        month, day, year = parts
-        return "{}-{}".format(year, month)
-    except:
-        raise ValueError("failed splitting date")
-
-
 def parse_statement(broker, pdf_path):
     # Load your PDF
     with open(pdf_path, "rb") as f:
         pdf = pdftotext.PDF(f)
-        pages = len(pdf)
+        txt = pdf[FIDELITY_ACCOUNT_VALUE_PAGE]
 
-    try:
-        # get account value & date
-        if IS_PCRA:
-            account_value_pdf_page = pdf[SCHWAB_PCRA_401K_ACCOUNT_VALUE_PAGE]
-        else: 
-            account_value_pdf_page = pdf[SCHWAB_ACCOUNT_VALUE_PAGE]
-        result = re.search(regex_store.ACCOUNT_VALUE, account_value_pdf_page) #using capture groups
-        date, account_value = result.groups()
-        sort_friendly_date = parse_date(date)
+        # get the 2nd line; ex: "February 1, 2022 - February 28, 2022"
+        date_builder = []
+        is_on_second_line = False
+        for i in range(100):
+            char = txt[i]
+            if char == "\n":
+                if is_on_second_line: 
+                    break
+                else: 
+                    is_on_second_line = True
+            if is_on_second_line:
+                date_builder.append(char)
+        
+        raw_date = "".join(date_builder)
+        ending_date = raw_date.split("-")[1] #the latter half
 
-    except ValueError as err:
-        print("Error with account value, date")
-        print(err.args)
+        formatted_date = datetime.datetime.strptime(ending_date, ' %B %d, %Y').strftime('%Y-%m')
 
-    try:
-        for page in range(pages - 1, -1, -1):
-            text = pdf[page]
 
-            withdrawal_amount = 0
-            deposit_amount = 0
 
-            digit = "[($]?[\d,]*\.\d\d\)?"
-            
-            purchases_sales = re.findall("Transaction Detail - Purchases & Sales", text)
-            deposits = re.findall("The total deposits activity for the statement period was ({})".format(digit), text)
-            withdrawals = re.findall("The total withdrawals activity for the statement period was ({})".format(digit), text)
+        regex = "Beginning Account Value\n(Additions\n)?(Subtractions\n)?(Transaction Costs, Fees & Charges\n)?Change in Investment Value[\* ]*\nEnding Account Value[\* ]*\n(Accrued Interest \(AI\)\n)?(Ending Account Value Incl. AI\n)?"
 
-            if withdrawals and deposits:
-                withdrawal_amount = withdrawals[0]
-                deposit_amount = deposits[0]
-                break
+        line_entries = re.findall(regex, txt)
 
-            if purchases_sales:
-                # the statement doesn't have a section for Deposits / withdrawals; stop parsing
-                break
+        # print(line_entries)
+        subtractions_entry = False
+        additions_entry = False
+        transactions_costs_fees_charges_entry = False
+        accured_interest_entry = False
+        ending_account_val_including_accured_interest_entry = False
 
-    except ValueError as err:
-        print("deposit / withdraw")
-        print(err.args)
 
-    bookkeep_month_entry(broker, sort_friendly_date, account_value, deposit_amount, withdrawal_amount)
+        if len(line_entries) == 0:
+            # the statement has a different format; try other regex
+            regex = "Beginning Account Value|Additions|Subtractions|Transaction Costs, Fees & Charges|Change in Investment Value|Ending Account Value|Accrued Interest \(AI\)|Ending Account Value Incl. AI"
+
+            line_entries = re.findall(regex, txt)
+        else:
+            line_entries = line_entries[0]
+
+        # determine if there is an entry for subtractions, additions, and transactions_costs_fees_charges
+        for entry in line_entries:
+            entry = entry.replace("\n", "")
+
+            if entry == "Additions":
+                additions_entry = True
+            elif entry == "Subtractions":
+                subtractions_entry = True
+            elif entry == "Transaction Costs, Fees & Charges":
+                # note, this debit is already included in subtractions
+                transactions_costs_fees_charges_entry = True
+            elif entry == "Accrued Interest (AI)":
+                accured_interest_entry = True
+            elif entry == "Ending Account Value Incl. AI":
+                ending_account_val_including_accured_interest_entry = True
+
+        # grab the #s associated with each entry
+        entry_data_regex = "This Period\n\nYear-to-Date\n\n([\s\S]*)$"
+        target_text = re.findall(entry_data_regex, txt)
+        
+        if not target_text:
+            # if "This Period" text is found elsewhere
+            entry_data_regex = "Year-to-Date\n\n([\s\S]*)$"
+            target_text = re.findall(entry_data_regex, txt)
+
+        digits_regex = "\-?\$?([\d,]*\.\d\d)|\n-\n"
+        # prepending "\n" to the search text is because the digits_regex will not pick up the first "-" unless it's there
+        # however, this is an edge case because only on the first account statement (there, the Beginning Account value
+        # is going to be 0, so it's represented with a "-")
+        digits = re.findall(digits_regex, "\n" + target_text[0])
+
+        # print(digits)
+        next_digit_iterator = iter(digits)
+
+
+        begn_account_val_this_period = next(next_digit_iterator)
+        begn_account_val_ytd = next(next_digit_iterator)
+
+        additions_this_period, additions_ytd = 0, 0
+        if additions_entry:
+            additions_this_period = next(next_digit_iterator)
+            additions_ytd = next(next_digit_iterator)
+        
+        subtractions_this_period, subtractions_ytd = 0, 0
+        if subtractions_entry:
+            subtractions_this_period = next(next_digit_iterator)
+            subtractions_ytd = next(next_digit_iterator)
+
+            if transactions_costs_fees_charges_entry:
+                transactions_costs_fees_charges_this_period = next(next_digit_iterator)
+                transactions_costs_fees_charges_ytd = next(next_digit_iterator)
+
+        change_in_investment_val_this_period = next(next_digit_iterator)
+        change_in_investment_val_ytd = next(next_digit_iterator)
+
+        ending_account_val_this_period = next(next_digit_iterator)
+        ending_account_val_ytd = next(next_digit_iterator)
+
+        # both of these should be true, but separating them just in case
+        accured_interest, ending_account_val_including_accured_interest = 0, 0
+        if accured_interest_entry:
+            accured_interest = next(next_digit_iterator)
+        if ending_account_val_including_accured_interest_entry:
+            ending_account_val_including_accured_interest = next(next_digit_iterator)
+
+
+        def verify_and_float_num(num):
+            if num:
+                return float(num)
+            else:
+                return 0
+
+        # note: the regex did not take into account (+) or (-) nums; this is because their signs are all intuitive
+        beginning_account_val = verify_and_float_num(begn_account_val_this_period)
+        account_val_change = verify_and_float_num(change_in_investment_val_this_period)
+        contributions = verify_and_float_num(additions_this_period)
+        withdrawals = verify_and_float_num(subtractions_this_period) * -1
+        account_val = verify_and_float_num(ending_account_val_this_period)
+
+        added_together = beginning_account_val + account_val_change + contributions + withdrawals
+        if round(added_together, 2) == account_val:
+            print("check")
+        else:
+            added_together = beginning_account_val - account_val_change + contributions + withdrawals
+            if round(added_together, 2) == account_val:
+                print("check 2")
+            else:
+                print("didn't add up")
+
+        print(contributions, withdrawals, account_val)
+
+    bookkeep_month_entry(broker, formatted_date, account_val, contributions, withdrawals)
 
 
 files = os.listdir(PATH_TO_BROKERAGE_STATEMENTS)
@@ -114,7 +197,7 @@ for file in files:
         if file[-4:] == ".pdf": #last 4 chars
             # continue
             print("now parsing {}, which is found at {}".format(file, PATH_TO_BROKERAGE_STATEMENTS + file))
-            parse_statement("Schwab", PATH_TO_BROKERAGE_STATEMENTS + file)
+            parse_statement("Fid", PATH_TO_BROKERAGE_STATEMENTS + file)
     except ValueError as err:
         print("opening file")
         print(err.args)
@@ -122,7 +205,7 @@ for file in files:
 
 header = ['year-month', 'deposits', 'withdrawals', 'ending_balance']
 
-with open('schwab-data.csv', 'w') as csv_writer:
+with open('data-fidelity.csv', 'w') as csv_writer:
     writer = csv.writer(csv_writer)
     writer.writerow(header)
 
